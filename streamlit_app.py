3cfb1294620cff4586c98231d0b8b3d4a4135f9b
Abhijeet Gorai <50688988+AbhijeetGorai@users.noreply.github.com>
2025-04-08 15:50:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,39 @@ import json
 st.title("📄 Document Processor")
 
 # --- Input fields ---
-email = st.text_input("📧 Email ID")
-access_token = st.text_input("🔐 Access Token", type="password")
-prompt = st.text_area("💬 Enter your prompt")
+email = "abhijeet.gorai@origamis.ai"
+access_token = "gAAAAABnhKC-u2n1_mSWDlroFECWdd_qqplTHfPnplQncjC0B4A-oSxMplEf117Zd0uXSmiJKX-hS9UalpqS3CkQDmvGbhhKIvvfBt4QiBgOliL7_vl_FncrR9YkqLOTg5cL0T3pBOeNYpy5kEXbdgH9jAPJWP2yBw=="
+prompt = """
+1.You are Finance Manager who oversee the reimbursement process in the company.
+2.Your task is to extract the following details from the Invoices:
+i)Type of Reimbursement - 
+If the Invoice is of a Flight, you will respond with "FLIGHT_REIMBURSEMENT".
+Similarly If the Invoice is of a Hotel, you will respond with "HOTEL_REIMBURSEMENT".
+Similarly you will handle all the different Invoice types.
+ii)Name of the Particulars - Passenger Name or Customer Name
+iii)Date of Journey -
+	->Start Date - Date of Journey
+	->End Date - Date of Reaching Destination
+iv)Time of Journey - 
+	->Start Time - Time of starting the journey on the date of journey
+	->End Time - Time of reaching the destination
+v)Total Cost - Total payable amount
+3.Generate a single JSON object in the below format only:
+{
+	"Type of Reimbursement" : {},
+	"Name of the Particulars" : {},
+	"Date of Journey" : [
+		"Start Date" :
+		"End Date" :
+	]
+	"Time of Journey" : [ 
+		"Start Time" :
+		"End Time" :
+	],
+	"Total Cost"
+}
+4. Remove the '\n' and '\t' characters from the output JSON structure
+"""
 
 # --- File uploader ---
 uploaded_files = st.file_uploader(
