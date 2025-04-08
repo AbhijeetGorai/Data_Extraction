e1ee4600dc8f30212cc31646055231559791a37d
Abhijeet Gorai <50688988+AbhijeetGorai@users.noreply.github.com>
2025-04-08 15:14:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if st.button("Generate"):
             }
 
             # API endpoint
-            api_url = "https://your-api-endpoint.com/process"  # 🔁 Replace this
+            api_url = "https://neptune.origamis.ai:9001/gear/process"  # 🔁 Replace this
 
             with st.spinner("Processing..."):
                 response = requests.post(api_url, data=data, files=files)
