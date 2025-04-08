b46ea410689db2d5cdf50e432ff382305f6dfb98
Abhijeet Gorai <50688988+AbhijeetGorai@users.noreply.github.com>
2025-04-08 21:40:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ For Example: In Invoices like cabs, customer name will be often written with a T
 For Example: If name is written like "manas", you will format it as "Manas".
 4)You will always keep Mr or Mrs as appropriate saluation in the front of the name.
 For Example: If name is written like "Mahendra", you will format it as "Mr Mahendra".
+5)You will always mention name in the format: <Salutation> <First Name> <Surname>
+For Example: If name is written like "Agarwal Manas", you will format it as "Mr Manas Agarwal".
 iii)Date of Journey -
 	->Start Date - Date of Journey
 	->End Date - Date of Reaching Destination
